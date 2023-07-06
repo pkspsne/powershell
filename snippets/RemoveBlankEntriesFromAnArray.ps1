7c8f9dc527599b0a29810495779321349d852e9f
pkspsne <132498283+pkspsne@users.noreply.github.com>
2023-07-06 11:29:08 -0400

    Update RemoveBlankEntriesFromAnArray.ps1

--- snippets/RemoveBlankEntriesFromAnArray.ps1
+++ snippets/RemoveBlankEntriesFromAnArray.ps1
@@ -1,27 +1,54 @@
 # RemoveBlankEntriesFromArray.ps1
-# By pkspsne
+# By pkspsne on 2023-07-06
 
-# Create an array with 5 elements, two of which are empty strings
+# Create an array with 5 elements, two of which are empty strings.
 $items = @("Something","Something else","Also something","","")
 
-# In PowerShell, an empty string will return false when used in an
-# If statement. 
+# If we print the $items array, we see it contains 5 elements.
+$items
+
+# OUTPUT:
+# Something
+# Something else
+# Also something
+# <blank line for empty string>
+# <blank line for empty string>
+
+# We can confirm the number of elements in the array by running:
+$items.Count
+
+# OUTPUT:
+# 5
+
+# In PowerShell, an empty string will return false when used as a
+# condition in an If statement:
 #
-# Example: $x = ""; If ($x) { # This will not execute }
+# Example: $x = ""
+#          If ($x) { # This will not execute }
 #
-# A non-empty string, meaning one that contains text, will return true
+# A non-empty string, meaning one that contains text, will return true:
 #
-# Example: $x = "Something"; If ($x) { # This WILL execute }
+# Example: $x = "Something"
+#          If ($x) { # This WILL execute }
 #
 # We can use this to remove empty strings from an array by going over
 # (iterating over) the array element by element and reassigning only strings
-# that return true back to the original variable.
+# that return true back to the original variable:
 
 $items = $items | ForEach-Object { If ($_) { $_ } }
 
 # Now if we print the $items array, it will only consist of the original
-# items which were not blank.
+# items which were not blank:
 
 $items 
 
-# $items = @("Something","Something else","Also something")
+# OUTPUT:
+# Something
+# Something else
+# Also something
+
+# We see the array has been recreated with only 3 elements now by running:
+$items.Count
+
+# OUTPUT:
+# 3
